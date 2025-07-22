db4127bb0d9ca24d7ed89ff63d801a19bf90750f
Nathan Dauny <nathan.dauny@capfi.fr>
2025-07-22 12:35:39 +0200

    first commit

---
+++ CsvToSqlInsertGenerator.cs
@@ -0,0 +1,17 @@
+﻿using CsvToDynamicObjectLib;
+
+namespace CsvToSqlInsert
+{
+    public class CsvToSqlInsertGenerator
+    {
+        private readonly ICsvToSqlConverter _converter;
+        public CsvToSqlInsertGenerator(ICsvToSqlConverter converter)
+        {
+            _converter = converter;
+        }
+        public IEnumerable<string> Generate(string tableName, CsvFinalObject csvData, Dictionary<string, Type> columnTypes)
+        {
+            return _converter.GenerateSqlStatements(tableName, csvData, columnTypes);
+        }
+    }
+}
---
+++ SqlInsertStatementBuilder.cs
@@ -0,0 +1,35 @@
+﻿using CsvToDynamicObjectLib;
+
+namespace CsvToSqlInsert
+{
+    public class SqlInsertStatementBuilder : ICsvToSqlConverter
+    {
+        public IEnumerable<string> GenerateSqlStatements(string tableName, CsvFinalObject csvData, Dictionary<string, Type> columnTypes)
+        {
+            foreach (var line in csvData)
+            {
+                var columns = string.Join(", ", line.Select(kv => kv.Key));
+
+                var values = line.Select(kv =>
+                {
+                    var colName = kv.Key;
+                    var value = kv.Value;
+                    var type = columnTypes[colName];
+
+                    if (value == null || string.IsNullOrEmpty(value.ToString()))
+                        return "NULL";
+
+                    if (type == typeof(string) || type == typeof(DateTime))
+                        return $"'{value.ToString().Replace("'", "''")}'";
+
+                    if (type == typeof(bool))
+                        return (bool)value ? "1" : "0";
+
+                    return value.ToString();
+                });
+
+                yield return $"INSERT INTO {tableName} ({columns}) VALUES ({string.Join(", ", values)});";
+            }
+        }
+    }
+}
